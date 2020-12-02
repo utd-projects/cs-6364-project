8c7a9a91810abd3778a721bbc076054d28b29e56
PratikBhusal <PratikBhusal@users.noreply.github.com>
2020-12-01 18:03:38 -0600

    Improve stratified train test split functionality

--- src/utils.py
+++ src/utils.py
@@ -5,62 +5,40 @@ import json
 import os
 from typing import Tuple
 
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-def stratified_train_test_split(
-    df: pd.DataFrame, column: str, test_size: float = 0.2
-) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """
-    Create stratified train-test split sampling on the given pandas datafram
-    based on the column given.
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        The pandas dataframe to perform the train-test split upon
-    column : str
-        The column to group by the different possible values.
-
-    Returns
-    -------
-    TODO
-
-    """
-    stratified_test = df.groupby("label").apply(lambda x: x.sample(frac=test_size))
-
-    return (
-        df[~df.apply(tuple, 1).isin(stratified_test.apply(tuple, 1))].reset_index(
-            drop=True
-        ),
-        stratified_test.reset_index(drop=True),
-    )
-
-
 if __name__ == "__main__":
-    stratified_mnist_train, stratified_mnist_test = stratified_train_test_split(
-        pd.read_csv(os.path.join("data", "raw", "mnist.csv")), "label"
+    mnist_df = pd.read_csv(os.path.join("data", "raw", "mnist.csv"))
+    stratified_mnist_train, stratified_mnist_test = train_test_split(
+        pd.read_csv(os.path.join("data", "raw", "mnist.csv")),
+        test_size=0.2,
+        stratify=mnist_df["label"],
     )
+    del mnist_df
 
     stratified_mnist_train.to_csv(
         os.path.join("data", "processed", "mnist_train.csv"), index=False
     )
+    del stratified_mnist_train
+
     stratified_mnist_test.to_csv(
         os.path.join("data", "processed", "mnist_test.csv"), index=False
     )
+    del stratified_mnist_test
 
-    (
-        stratified_fashion_mnist_train,
-        stratified_fashion_mnist_test,
-    ) = stratified_train_test_split(
-        pd.read_csv(os.path.join("data", "raw", "fashion-mnist.csv")), "label"
+    fashion_mnist_df = pd.read_csv(os.path.join("data", "raw", "fashion-mnist.csv"))
+    stratified_fashion_mnist_train, stratified_fashion_mnist_test = train_test_split(
+        fashion_mnist_df, test_size=0.2, stratify=fashion_mnist_df["label"],
     )
+    del fashion_mnist_df
 
     stratified_fashion_mnist_train.to_csv(
         os.path.join("data", "processed", "fashion-mnist_train.csv"), index=False
     )
+    del stratified_fashion_mnist_train
+
     stratified_fashion_mnist_test.to_csv(
         os.path.join("data", "processed", "fashion-mnist_test.csv"), index=False
     )
+    del stratified_fashion_mnist_test
