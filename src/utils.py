7dc781a0e10e5e8cabaeb0660264d0a427313348
PratikBhusal <PratikBhusal@users.noreply.github.com>
2020-12-09 12:35:32 -0600

    Change dataset split to be 70-10-20 (train-val-test)

--- src/utils.py
+++ src/utils.py
@@ -17,10 +17,25 @@ if __name__ == "__main__":
     )
     del mnist_df
 
+    stratified_mnist_train, stratified_mnist_val = train_test_split(
+        stratified_mnist_train,
+        train_size=0.875,
+        stratify=stratified_mnist_train["label"],
+    )
+
+    print(stratified_mnist_train)
+    print(stratified_mnist_val)
+    print(stratified_mnist_test)
+
     stratified_mnist_train.to_csv(
         os.path.join("data", "processed", "mnist_train.csv"), index=False
     )
     del stratified_mnist_train
+
+    stratified_mnist_val.to_csv(
+        os.path.join("data", "processed", "mnist_val.csv"), index=False
+    )
+    del stratified_mnist_val
 
     stratified_mnist_test.to_csv(
         os.path.join("data", "processed", "mnist_test.csv"), index=False
@@ -29,14 +44,31 @@ if __name__ == "__main__":
 
     fashion_mnist_df = pd.read_csv(os.path.join("data", "raw", "fashion-mnist.csv"))
     stratified_fashion_mnist_train, stratified_fashion_mnist_test = train_test_split(
-        fashion_mnist_df, test_size=0.2, stratify=fashion_mnist_df["label"],
+        fashion_mnist_df,
+        test_size=0.2,
+        stratify=fashion_mnist_df["label"],
     )
     del fashion_mnist_df
+
+    stratified_fashion_mnist_train, stratified_fashion_mnist_val = train_test_split(
+        stratified_fashion_mnist_train,
+        train_size=0.875,
+        stratify=stratified_fashion_mnist_train["label"],
+    )
+
+    print(stratified_fashion_mnist_train)
+    print(stratified_fashion_mnist_val)
+    print(stratified_fashion_mnist_test)
 
     stratified_fashion_mnist_train.to_csv(
         os.path.join("data", "processed", "fashion-mnist_train.csv"), index=False
     )
     del stratified_fashion_mnist_train
+
+    stratified_fashion_mnist_val.to_csv(
+        os.path.join("data", "processed", "fashion-mnist_val.csv"), index=False
+    )
+    del stratified_fashion_mnist_val
 
     stratified_fashion_mnist_test.to_csv(
         os.path.join("data", "processed", "fashion-mnist_test.csv"), index=False
